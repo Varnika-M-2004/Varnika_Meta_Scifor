44ed77ed8e1a1bd1feb8fca8713eff6588d04f2e
Varnika-M-2004 <88385934+Varnika-M-2004@users.noreply.github.com>
2024-06-02 01:29:34 +0530

    Update merge-strings-alternately.py

--- LeetCode Python Solutions/1894-merge-strings-alternately/merge-strings-alternately.py
+++ LeetCode Python Solutions/1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,18 +5,23 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
+        #check the length of the words and store them in variables
         len1 = len(word1)
         len2 = len(word2)
+        #initialize a variable total to save the final word
         total = ""
+        #alsi initialize variables i and j, both of which will iterate over word1 and word2 resp.
         i = 0
         j = 0
+        #while we don't reach the end of the strings we will iterate
         while i<len1 and j<len2:
+            #in this the total variable will store the alternate words from word1 and word2
             total += word1[i] + word2[j]
+            #increment values of i and j by 1
             i+=1
             j+=1
         
-        '''if any word is longer than the other, add the remaining characters
-        to the end of the final string'''
+        #check if any word is longer than the other, and add the left out words to the end of the final word 'total''''
         total += word1[i:] + word2[j:]
         return total
         
