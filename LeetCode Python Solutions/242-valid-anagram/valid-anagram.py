07b127eee3eaf2b6919e676e9ba4c7012e4c4e6c
Varnika-M-2004 <88385934+Varnika-M-2004@users.noreply.github.com>
2024-06-02 01:36:05 +0530

    Update valid-anagram.py

--- LeetCode Python Solutions/242-valid-anagram/valid-anagram.py
+++ LeetCode Python Solutions/242-valid-anagram/valid-anagram.py
@@ -5,6 +5,9 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        '''in this we use the python in-built function called sorted(). It will check whether the characters are same in 
+        both the words irrespective of their arrangement. If yes, then it will return true, else will return false
+        '''
         if sorted(s) == sorted(t):
             return True
         else:
