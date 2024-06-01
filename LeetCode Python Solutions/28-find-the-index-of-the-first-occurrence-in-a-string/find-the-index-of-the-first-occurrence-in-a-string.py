c40e30851b5c0e0660ef1fc73cd4c6782891c846
Varnika-M-2004 <88385934+Varnika-M-2004@users.noreply.github.com>
2024-06-02 01:38:29 +0530

    Update find-the-index-of-the-first-occurrence-in-a-string.py

--- LeetCode Python Solutions/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ LeetCode Python Solutions/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,6 +5,11 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        '''in this we use python's in-built function find() to get the index value of the 
+        first occurence of the provided subset of the string from the main string.
+        It will return the index value of the first occurence of the subset of the string if it is 
+        present in the main string else it'll return -1.
+        '''
         index_value = haystack.find(needle)
         return index_value
         
