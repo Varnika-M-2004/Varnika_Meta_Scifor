0e03b68cc3b509f5ce10333e21ef57eb61a95add
Varnika-M-2004 <88385934+Varnika-M-2004@users.noreply.github.com>
2024-06-02 01:41:54 +0530

    Update monotonic-array.py

--- LeetCode Python Solutions/932-monotonic-array/monotonic-array.py
+++ LeetCode Python Solutions/932-monotonic-array/monotonic-array.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+
+        '''In this, we use the sorted() in-built python function to first check whether the array is sorted or not.
+        If it is not sorted in ascending order, it will use the reverse=True to check whether the array is sorted in the descending order.
+        If it satisfies either of the conditions, the function will return True. Else it will return False.
+        '''
         if nums == sorted(nums) or nums==sorted(nums, reverse=True):
             return True
         else:
