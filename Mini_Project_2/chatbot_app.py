210f857569896f64059d4f6fbb66b26f31a12474
Varnika Mulay <88385934+Varnika-M-2004@users.noreply.github.com>
2024-08-08 19:20:14 +0530

    Update chatbot_app.py

--- Mini_Project_2/chatbot_app.py
+++ Mini_Project_2/chatbot_app.py
@@ -18,10 +18,13 @@ st.markdown(
     """
     <style>
     .title {
-        color: white;  
-        font-size: 40px; 
+        color: teal;
+        font-size: 45px;
+        text-align: center;
+        font-family: verdana;
     }
     </style>
+    <h1><p class="title">🤔<b><i> Gemini API Chatbot</i></b> 🤔</p></h1>
     """,
     unsafe_allow_html=True
 )
@@ -30,54 +33,32 @@ st.markdown(
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Set up and define the Google Gemini AI model
-GOOGLE_API_KEY = st.secrets["gemini"]["api_key"]
-# Initialize the Google API key
 gen_ai.configure(api_key=GOOGLE_API_KEY)
 model = gen_ai.GenerativeModel('gemini-pro')
 
-# Create a function to translate roles between Gemini-Pro and Streamlit terminology
-def translate_roles(user_role):
-    if user_role == 'model':
-        return "assistant"
-    else:
-        return user_role
-
-# Initialize a chat session in Streamlit if not already present
+# Initialize the chat session and welcome message in session state
 if "chat_session" not in st.session_state:
     st.session_state.chat_session = model.start_chat(history=[])
-
-# Display the chatbot's title on the page with custom styling
-#st.markdown('<h1><p class="title">🤔Gemini API Chatbot🤔</p></h1>', unsafe_allow_html=True)
-st.markdown(
-    """
-    <style>
-    .title {
-        color: teal;
-        font-size: 45px;
-        text-align: center;
-        font-family:verdana;
-    }
-    </style>
-    <h1><p class="title">🤔<b><i> Gemini API Chatbot</i></b> 🤔</p></h1>
-    """,
-    unsafe_allow_html=True
-)
-
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello! Welcome to Gemini Chatbot! How may I help you today?"}
+    ]
 
 # Display the chat history
-for message in st.session_state.chat_session.history:
-    with st.chat_message(translate_roles(message.role)):
-        st.markdown(message.parts[0].text)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # Input field for user input
 user_prompt = st.chat_input("Enter your prompt...")
 if user_prompt:
     # Add user's question to chat and display it
+    st.session_state.messages.append({"role": "user", "content": user_prompt})
     st.chat_message("user").markdown(user_prompt)
 
     # Send user's question to Gemini-Pro and get answer
     gemini_answer = st.session_state.chat_session.send_message(user_prompt)
 
     # Display the answer on the screen
+    st.session_state.messages.append({"role": "assistant", "content": gemini_answer.text})
     with st.chat_message("assistant"):
         st.markdown(gemini_answer.text)
